9d16a32e744545550aa06736cf07ddb95acbcc31
Lars Kölpin <lrsklpn@live.de>
2020-06-15 16:43:30 +0200

    feat(fire): Add Fire Events

--- pkg/domain/fireEvents.go
+++ pkg/domain/fireEvents.go
@@ -5,70 +5,70 @@ import (
 	"github.com/markus-wa/demoinfocs-golang/v2/pkg/demoinfocs/events"
 )
 
-func SmokeStarted(tick int, e events.SmokeStart) GameEvent {
-	return GameEvent{
-		Name: "SMOKE_STARTED",
-		RealEvent: SmokeStartedEvent{
-			Position: Position{
-				X: e.Position.X,
-				Y: e.Position.Y,
-				Z: e.Position.Z,
-			},
-			Name: "SMOKE_STARTED",
-			Tick: tick,
-		},
-	}
-}
-
-func SmokeExpired(tick int, e events.SmokeExpired) GameEvent {
-	return GameEvent{
-		Name: "SMOKE_EXPIRED",
-		RealEvent: SmokeExpiredEvent{
-			Position: Position{
-				X: e.Position.X,
-				Y: e.Position.Y,
-				Z: e.Position.Z,
-			},
-			Name: "SMOKE_EXPIRED",
-			Tick: tick,
-		},
-	}
-}
-
-type SmokeStartedEvent struct {
-	Position Position `json:"position"`
-	Name     string   `json:"name"`
-	Tick     int   `json:"tick"`
-}
-
-type SmokeExpiredEvent struct {
-	Position Position `json:"position"`
-	Name     string   `json:"name"`
+type FireStartedEvent struct {
+  Position Position `json:"position"`
+  Name     string   `json:"name"`
   Tick     int   `json:"tick"`
 }
 
-var SmokeStartedType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "SmokeStarted",
+type FireExpiredEvent struct {
+  Position Position `json:"position"`
+  Name     string   `json:"name"`
+  Tick     int   `json:"tick"`
+}
+
+func FireStarted(tick int, e events.SmokeStart) GameEvent {
+	return GameEvent{
+		Name: "FIRE_STARTED",
+		RealEvent: FireStartedEvent{
+			Position: Position{
+				X: e.Position.X,
+				Y: e.Position.Y,
+				Z: e.Position.Z,
+			},
+			Name: "FIRE_STARTED",
+			Tick: tick,
+		},
+	}
+}
+
+func FireExpired(tick int, e events.SmokeExpired) GameEvent {
+	return GameEvent{
+		Name: "FIRE_EXPIRED",
+		RealEvent: FireExpiredEvent{
+			Position: Position{
+				X: e.Position.X,
+				Y: e.Position.Y,
+				Z: e.Position.Z,
+			},
+			Name: "FIRE_EXPIRED",
+			Tick: tick,
+		},
+	}
+}
+
+var FireStartedType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "FireStarted",
 	Fields: graphql.Fields{
 		"name": &graphql.Field{
 			Name: "name",
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "SMOKE_STARTED", nil
+				return "FIRE_STARTED", nil
 			},
 		},
 		"position": &graphql.Field{
 			Type: PositionType,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				x := p.Source.(GameEvent)
-				return x.RealEvent.(SmokeStartedEvent).Position, nil
+				return x.RealEvent.(FireStartedEvent).Position, nil
 			},
 		},
     "tick": &graphql.Field{
       Type: graphql.Int,
       Resolve: func(p graphql.ResolveParams) (interface{}, error) {
         x := p.Source.(GameEvent)
-        return x.RealEvent.(SmokeStartedEvent).Tick, nil
+        return x.RealEvent.(FireStartedEvent).Tick, nil
       },
     },
 	},
@@ -79,34 +79,34 @@ var SmokeStartedType = graphql.NewObject(graphql.ObjectConfig{
 	},
 })
 
-var SmokeExpiredType = graphql.NewObject(graphql.ObjectConfig{
-	Name: "SmokeExpired",
+var FireExpiredType = graphql.NewObject(graphql.ObjectConfig{
+	Name: "FireExpired",
 	Fields: graphql.Fields{
 		"name": &graphql.Field{
 			Name: "name",
 			Type: graphql.String,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
-				return "SMOKE_EXPIRED", nil
+				return "FIRE_EXPIRED", nil
 			},
 		},
 		"position": &graphql.Field{
 			Type: PositionType,
 			Resolve: func(p graphql.ResolveParams) (interface{}, error) {
 				x := p.Source.(GameEvent)
-				return x.RealEvent.(SmokeExpiredEvent).Position, nil
+				return x.RealEvent.(FireExpiredEvent).Position, nil
 			},
 		},
     "tick": &graphql.Field{
       Type: graphql.Int,
       Resolve: func(p graphql.ResolveParams) (interface{}, error) {
         x := p.Source.(GameEvent)
-        return x.RealEvent.(SmokeExpiredEvent).Tick, nil
+        return x.RealEvent.(FireExpiredEvent).Tick, nil
       },
     },
 	},
 	IsTypeOf: func(p graphql.IsTypeOfParams) bool {
 		eventName := p.Value.(GameEvent).Name
 
-		return eventName == "SMOKE_EXPIRED"
+		return eventName == "FIRE_EXPIRED"
 	},
 })
