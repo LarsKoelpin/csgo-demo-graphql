c23e97066af22a22df230fc9a2628397a9be5a6c
Lars Kölpin <lrsklpn@live.de>
2020-06-15 15:59:38 +0200

    Updates

---
+++ precache-manifest.b0ff4832d1d5d72efa1407c4c86f71f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26aa2f4dfb26522c82d1e07314cc1282",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e9cb04366f36c858a7e7",
+    "url": "/static/css/2.ae68f578.chunk.css"
+  },
+  {
+    "revision": "e9cb04366f36c858a7e7",
+    "url": "/static/js/2.3031ad33.chunk.js"
+  },
+  {
+    "revision": "da3a576957a45fd9b06cc89fba9d88e6",
+    "url": "/static/js/2.3031ad33.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30fa8a732cd117aaf843",
+    "url": "/static/js/main.88d035dd.chunk.js"
+  },
+  {
+    "revision": "936d072e5d88179470a5",
+    "url": "/static/js/runtime-main.85886201.js"
+  }
+]);
