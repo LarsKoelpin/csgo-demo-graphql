e91232c231792d5dde450e7ff8a71fddd08c6f37
Lars Kölpin <lrsklpn@live.de>
2020-06-15 15:56:11 +0200

    docs(api-explorer): Add interactive API Explorer

---
+++ docs/api-explorer/src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import "graphiql/graphiql.min.css";
+import {GraphiQL} from "graphiql";
+import {mockServer} from "graphql-tools";
+import {buildClientSchema, printSchema} from "graphql/utilities";
+import {schema} from "./schema";
+
+const defaultQuery = `
+{
+  demo(freq: 0.2, demoFile: "asd") {
+    header {
+      mapName
+    }
+    ticks {
+      participants {
+        entityId
+
+      }
+    }
+  }
+}
+`;
+
+const x = buildClientSchema(schema.data);
+const rawSchema = printSchema(x);
+
+const myMockServer = mockServer(x, {
+  demo: () => ({
+    header: {
+      mapName: "abc",
+      snapshotRate: 0.2
+    },
+    ticks: [
+      {
+        participants: [
+          {
+            entityId: 7,
+            position: {
+              x: -1871.7869873046875
+            },
+            team: 2
+          }
+        ]
+      }
+    ]
+  })
+});
+
+const fetcher = (abc) => {
+  return myMockServer.query(abc.query);
+};
+
+
+export function App() {
+  return (<GraphiQL
+      schema={x}
+      fetcher={fetcher}
+      defaultQuery={defaultQuery}
+    />
+  );
+}
