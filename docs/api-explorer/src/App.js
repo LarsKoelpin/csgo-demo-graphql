df2604f227651b768a91e9910e41476e0a17ac37
Lars Kölpin <lrsklpn@live.de>
2020-06-15 19:14:21 +0200

    docs: Update default query

--- docs/api-explorer/src/App.js
+++ docs/api-explorer/src/App.js
@@ -2,47 +2,38 @@ import React from 'react';
 import "graphiql/graphiql.min.css";
 import {GraphiQL} from "graphiql";
 import {mockServer} from "graphql-tools";
-import {buildClientSchema, printSchema} from "graphql/utilities";
+import {buildClientSchema} from "graphql/utilities";
 import schema from "./schema.json";
+import data from './data.json';
 
 const defaultQuery = `
 {
-  demo(freq: 0.2, demoFile: "asd") {
+  demo(freq: 0.2) {
     header {
       mapName
     }
     ticks {
       participants {
-        entityId
-
+        name
+        armor
+        angleX
+        angleY
+        equipment {
+          ammoInMagazine
+          ammoReserve
+          type
+        }
       }
     }
   }
 }
+
 `;
 
 const x = buildClientSchema(schema.data);
 
 const myMockServer = mockServer(x, {
-  demo: () => ({
-    header: {
-      mapName: "abc",
-      snapshotRate: 0.2
-    },
-    ticks: [
-      {
-        participants: [
-          {
-            entityId: 7,
-            position: {
-              x: -1871.7869873046875
-            },
-            team: 2
-          }
-        ]
-      }
-    ]
-  })
+  demo: () => data.data.demo
 });
 
 const fetcher = (abc) => {
